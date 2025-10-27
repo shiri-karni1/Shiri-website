a92db35ae090e6c8e5858fb1cd557cf38990d3a3
Shiri Karni <shiri@innovate.huji.ac.il>
2025-10-27 18:44:54 +0200

    changed botton activity color and special class background color to better fit the website theme

--- main.js
+++ main.js
@@ -1,31 +1,14 @@
+
+const myPage = document.getElementById("fullbody");
 const button = document.getElementById("firstButton");
+
 button.addEventListener("click", () => {
-  if (button.classList.contains("special")) {
-    button.classList.remove("special");
-  } else {
+  if (myPage.classList.contains("special")) {
+    myPage.classList.remove("special");
     button.classList.add("special");
+  
+  } else {
+    myPage.classList.add("special");
+    button.classList.remove("special");
   }
 });
-
-
-console.log("Hello World!");
-setupCounter();
-
-function setupCounter() {
-  let count = 0;
-
-  function increment() {
-    count++;
-    document.querySelector("#count").innerHTML = count;
-  }
-
-  function decrement() {
-    count--;
-    document.querySelector("#count").innerHTML = count;
-  }
-
-  document.querySelector("#increment").addEventListener("click", increment);
-  document.querySelector("#decrement").addEventListener("click", decrement);
-}
-
-
